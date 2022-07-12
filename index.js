4e6a7d374e7bfb199246cf0aaab4b345987d6f5f
Lucas Duncan <lucasdonuts@gmail.com>
2022-07-12 14:49:31 -0500

    commit tree 7828fa8cef0b253384bfb2dc155719993d7586f3

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(driverNames, string) {
+  return driverNames.filter(name => name.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(driverNames, string) {
+  return driverNames.filter(name => name.toLowerCase().startsWith(string.toLowerCase()));
+}
+
+function matchName(driverObjects, string) {
+  return driverObjects.filter(driver => driver.name.toLowerCase() === string.toLowerCase());
+}
